2f32399f1728a37063c5a6fe14c1b85ff3e78f1c
jimmyoz <12707306@qq.com>
2022-05-24 14:19:06 +0800

    Add voyager_service
    20220524

---
+++ voyager_service_2.4/service_install.bat
@@ -0,0 +1,21 @@
+@echo off
+title voyager.bat
+echo Administrator
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+if %errorlevel%==0 goto Admin
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" /f
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
+
+:Admin
+echo Administrator success
+
+cd /d %~dp0
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe %~dp0voyagerService.exe
+Net Start voyager 
+sc config voyager start= auto
+pause
